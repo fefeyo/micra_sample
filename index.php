21016263c21656a26b8ca683810efe70ef9f3c13
Sakata Yu <imanity.fefe@gmail.com>
2017-06-30 00:10:13 +0900

    initial_commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>ふぇふぇのサーバー</title>
+    </head>
+    <body>
+        <h1>こんにちは！<?php echo exec('whoami'); ?>です。</h1>
+        <div class="main">
+            ここにサーバーの設定画面を作る予定だよ
+        </div>
+    </body>
+</html>
